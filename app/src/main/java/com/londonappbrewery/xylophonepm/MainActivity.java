0cd9ef1db696d11c6fcea37f8b08f74a18cab823
ZainLaptop <zain.ul_abdeen1191@live.ie>
2018-03-19 20:09:34 +0000

    Committing changes

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private final int PRIORITY = 0;
     private final float NORMAL_PLAY_RATE = 1.0f;
 
-    // TODO: Add member variables here
+
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -25,21 +26,69 @@ public class MainActivity extends AppCompatActivity {
     private int mASoundId;
     private int mBSoundId;
 
+    private SoundPool mSoundPool;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Create a new SoundPool
+
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
+
 
 
         // TODO: Load and get the IDs to identify the sounds
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId = mSoundPool.load(getApplicationContext(),R.raw.note5_g,1);
+        mASoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a,1);
+        mBSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b,1);
+
+
 
 
     }
 
-    // TODO: Add the play methods triggered by the buttons
 
+
+    public void playC(View view){
+        Log.d("music app","Red");
+        mSoundPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
+
+    public void playD(View view){
+        Log.d("music app","Orange");
+        mSoundPool.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    public void playE(View view){
+        Log.d("music app","Yellow");
+        mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    public void playF(View view){
+        Log.d("music app","Green");
+        mSoundPool.play(mFSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    public void playG(View view){
+        Log.d("music app","Turquoise");
+        mSoundPool.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    public void playA(View view){
+        Log.d("music app","Blue");
+        mSoundPool.play(mASoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+
+    public void playB(View view){
+        Log.d("music app","Purple");
+        mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
 
 
 }
